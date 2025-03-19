11d0dfb7161df245c05338ded4afcf37c4760642
peterpontocom <willpontocom07@gmail.com>
2025-03-19 14:53:21 +0100

    feat:convert welcome section to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/layout/header";
+import { WelcomeSection } from "@/components/layout/welcome-section";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Send } from "lucide-react";
@@ -8,16 +9,12 @@ export default function Home() {
     <div className="grid h-[700px] grid-rows-[min-content_1fr_min-content] gap-4 p-5 py-4 md:h-[600px] md:px-10 lg:h-[600px] lg:px-20">
       <Header />
       <main className="flex items-center">
-        <h1 className="w-full self-center text-center text-xl font-medium lg:text-2xl">
-          <span className="font-semibold">Hey, Peter</span>
-          <br />
-          <span className="text-muted-foreground">Como posso ajudar hoje?</span>
-        </h1>
+        <WelcomeSection />
       </main>
       <footer className="">
         <div className="flex items-center gap-2">
           <Input placeholder="Enviar mensagem" className="p-5" />
-          <Button className="bg-primary"> 
+          <Button className="bg-primary">
             <Send />
           </Button>
         </div>
---
+++ src/components/layout/welcome-section.tsx
@@ -0,0 +1,9 @@
+export function WelcomeSection() {
+  return (
+    <h1 className="w-full self-center text-center text-xl font-medium lg:text-2xl">
+      <span className="font-semibold">Hey, Peter</span>
+      <br />
+      <span className="text-muted-foreground">Como posso ajudar hoje?</span>
+    </h1>
+  );
+}
